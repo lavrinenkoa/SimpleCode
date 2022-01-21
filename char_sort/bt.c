412b853c6edfbef48c0f9ab6ff1ab92a78e7f042
Alexandr <allavrinenko@gmail.com>
2022-01-21 17:44:42 +0300

    Update bt.c

--- char_sort/bt.c
+++ char_sort/bt.c
@@ -4,7 +4,7 @@
 
 void sort(char *data, int n)
 {
-	char table[256] = { 0 };
+	char table[sizeof(char)] = { 0 };
 	int i, j, p = 0;
 
 	printf("in:  %s\n", data);
@@ -14,7 +14,7 @@ void sort(char *data, int n)
 		table[data[i]]++;
 	}
 
-	for (i = 0; i < 256; i++) {
+	for (i = 0; i < sizeof(char); i++) {
 //		printf("%d\t%d\n", m[i], i);
 		for (j = 0; j < table[i]; j++) {
 			data[p++] = i;
