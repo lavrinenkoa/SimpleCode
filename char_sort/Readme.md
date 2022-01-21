c5852eb5d03ec65cbefaa7e7f9bcd3dd23771950
Alexander <allavrinenko@gmail.com>
2022-01-21 13:24:51 +0300

    Add char string sorting example

---
+++ char_sort/Readme.md
@@ -0,0 +1,9 @@
+Build and run result:
+
+$ make && ./bt
+gcc -g -rdynamic  -I/usr/include/ -I.. -c bt.c
+gcc -g -rdynamic  -I/usr/include/ -I.. bt.o -o bt -lrt
+
+in:  eeaaaaabbbaccccccdddddddaaaaaddddbbbbaaaa
+out: aaaaaaaaaaaaaaabbbbbbbccccccdddddddddddee
+
---
+++ char_sort/bt.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void sort(char *data, int n)
+{
+	char table[256] = { 0 };
+	int i, j, p = 0;
+
+	printf("in:  %s\n", data);
+
+	for (i = 0; i < n; i++) {
+//		printf("%c\t%d\n", data[i] , data[i]);
+		table[data[i]]++;
+	}
+
+	for (i = 0; i < 256; i++) {
+//		printf("%d\t%d\n", m[i], i);
+		for (j = 0; j < table[i]; j++) {
+			data[p++] = i;
+		}
+	}
+
+	printf("out: %s\n", data);
+}
+
+int main(int argc, char *argv[]) {
+	char data[] = "eeaaaaabbbaccccccdddddddaaaaaddddbbbbaaaa";
+
+	sort(data, strlen(data));
+
+	exit(EXIT_SUCCESS);
+}
